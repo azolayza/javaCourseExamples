ba58c64fb1cf77922936a0bf520cd684ddc9dd1f
azolayza <azovcevae@gmail.com>
2021-02-22 18:51:45 +0700

    lesson 4.6 изменен тест на удаление групп
    Сравнение коллекций а не их размеров

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -15,11 +15,15 @@ public class GroupDeletionTests extends TestBase{
       app.getGroupHelper().createGroup(new GroupData("test11", null, "testgroup"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size()-2);
+    app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size()-1);
+
+    before.remove(before.size()-1);
+    Assert.assertEquals(before, after);
   }
+
 
 }
