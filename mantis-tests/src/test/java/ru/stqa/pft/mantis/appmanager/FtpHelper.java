c0f03fe41787d41c7bce9267f1d5f5fd3406b10f
azolayza <azovcevae@gmail.com>
2021-03-27 17:31:45 +0700

    Лекция 8.6. Получение почты на встроенный в тесты почтовый сервер
    ТЕсты падают на ftp upload

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/FtpHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/FtpHelper.java
@@ -1,9 +1,9 @@
 package ru.stqa.pft.mantis.appmanager;
 
 import org.apache.commons.net.ftp.FTPClient;
-
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class FtpHelper {
   private final ApplicationManager app;
@@ -14,7 +14,7 @@ public class FtpHelper {
     ftp = new FTPClient();
   }
 
-  public void upload(File file,String target, String backup) throws Exception {
+  public void upload(File file,String target, String backup) throws IOException {
     ftp.connect(app.getProperty("ftp.host"));
     ftp.login(app.getProperty("ftp.login"), app.getProperty("ftp.password"));
     ftp.deleteFile(backup);
@@ -24,7 +24,7 @@ public class FtpHelper {
     ftp.disconnect();
   }
 
-  public void restore(String backup, String target) throws Exception {
+  public void restore(String backup, String target) throws IOException {
     ftp.connect(app.getProperty("ftp.host"));
     ftp.login(app.getProperty("ftp.login"), app.getProperty("ftp.password"));
     ftp.deleteFile(backup);
