64ae6b075a0033b36d64b9c1371c57d78b8c1e62
azolayza <azovcevae@gmail.com>
2021-04-04 20:50:20 +0700

    ДЗ 20 Интеграция тестов с баг-трекером Bugify
    В модуле rest-sample добавлены IssueHelper, модель Issue для посмотрения
    списка задач
    В тест testCreateIssue добавлен шаг проверки статуса задачи

--- rest-sample/src/test/java/ru/stqa/pft/rest/Issue.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/Issue.java
@@ -7,6 +7,16 @@ public class Issue {
   private int id;
   private String subject;
   private String description;
+  private String state_name;
+
+  public String getStateName() {
+    return state_name;
+  }
+
+  public Issue withState_name(String state_name) {
+    this.state_name = state_name;
+    return this;
+  }
 
   public int getId() {
     return id;
@@ -36,15 +46,25 @@ public class Issue {
   }
 
   @Override
+  public String toString() {
+    return "Issue{" +
+            "id=" + id +
+            ", subject='" + subject + '\'' +
+            ", description='" + description + '\'' +
+            ", state_name='" + state_name + '\'' +
+            '}';
+  }
+
+  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (!(o instanceof Issue)) return false;
     Issue issue = (Issue) o;
-    return getId() == issue.getId() && Objects.equals(getSubject(), issue.getSubject()) && Objects.equals(getDescription(), issue.getDescription());
+    return getId() == issue.getId() && Objects.equals(getSubject(), issue.getSubject()) && Objects.equals(getDescription(), issue.getDescription()) && Objects.equals(getStateName(), issue.getStateName());
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(getId(), getSubject(), getDescription());
+    return Objects.hash(getId(), getSubject(), getDescription(), getStateName());
   }
 }
