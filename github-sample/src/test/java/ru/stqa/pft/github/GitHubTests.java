cda2d14ac4e43905f5613b2e7c098160cffc2436
azolayza <azovcevae@gmail.com>
2021-04-05 23:47:19 +0700

    9.6. REST: взаимодействие с GitHub
    Тест вывода списка коммитов из личного репо

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GitHubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GitHubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("azolayza", "javaCourseExamples")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
